568284a74e3dc6bcd60a3156cb9407b3f6706e7d
Batoh67 <vbanszel@gmail.com>
2025-12-05 12:54:21 +0100

    updated Joint class

--- 3D ForceCalculationStatic.py
+++ 3D ForceCalculationStatic.py
@@ -6,3 +6,13 @@ class Joint:
         self.x = x
         self.y = y
         self.z = z
+
+    def __repr__(self) -> str:
+        return f"Joint(x={self.x}, y={self.y}, z={self.z})"
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y}, {self.z})"
+
+    def get_coordinates(self) -> Tuple[float, float, float]:
+        """Return coordinates as a tuple."""
+        return self.x, self.y, self.z
